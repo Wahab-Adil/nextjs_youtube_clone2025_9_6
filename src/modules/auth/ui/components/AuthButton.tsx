3be9eea4f9f8cf544b4e5909bde37c08aca5b594
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-09-07 21:53:39 +0430

    Create AuthButton.tsx

---
+++ src/modules/auth/ui/components/AuthButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { UserCircle } from "lucide-react";
+
+import { UserButton, SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
+
+export const AuthButton = () => {
+  return (
+    <>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none"
+          >
+            <UserCircle className="size-4" />
+            <span>Sign In</span>
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
+  );
+};
