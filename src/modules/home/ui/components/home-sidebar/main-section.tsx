fdc12800de944d92eba0924ca84ff2d8f065520a
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-09-07 21:54:05 +0430

    Create main-section.tsx

---
+++ src/modules/home/ui/components/home-sidebar/main-section.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { useClerk, useAuth } from "@clerk/nextjs";
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+import { Home, PlaySquare, Flame } from "lucide-react";
+
+const items = [
+  { title: "Home", url: "/", icon: Home },
+  {
+    title: "Subscriptions",
+    url: "/feed/subscriptions",
+    icon: PlaySquare,
+    auth: true,
+  },
+  { title: "Trending", url: "/feed/trending", icon: Flame },
+];
+
+export const MainSection = () => {
+  const clerk = useClerk();
+  const { isSignedIn } = useAuth();
+
+  return (
+    <SidebarGroup>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => {
+            return (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton
+                  tooltip={item.title}
+                  asChild
+                  isActive={false}
+                  onClick={(e) => {
+                    if (!isSignedIn && item.auth) {
+                      e.preventDefault();
+                      return clerk.openSignIn();
+                    }
+                  }}
+                >
+                  <Link href={item.url}>
+                    <item.icon />
+                    <span className="flex items-center gap-4 text-sm">
+                      {item.title}
+                    </span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            );
+          })}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+};
