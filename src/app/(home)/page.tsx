0f8710cc654e608a437259b70c02a38d4811825d
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-09-09 22:31:18 +0430

    trpc config

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+import { trpc } from "../trpc/client";
+
 export default function Home() {
-  return <div className="pl-3 font-bold ">This is Main Section</div>;
+  const { data } = trpc.hello.useQuery({ text: "wahab" });
+  return <div className="pl-3 font-bold ">This is Main {data?.greeting}</div>;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import { TRPCProvider } from "./trpc/client";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
@@ -17,9 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider afterSignOutUrl="/">
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
+      <TRPCProvider>
+        <html lang="en">
+          <body className={inter.className}>{children}</body>
+        </html>
+      </TRPCProvider>
     </ClerkProvider>
   );
 }
