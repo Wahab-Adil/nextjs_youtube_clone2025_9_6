c5046c4d6bca7788e5e6ea23efe34d67ef50051c
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-09-10 21:38:45 +0430

    Clerk Auth Added to tRPC config

--- src/app/trpc/init.ts
+++ src/app/trpc/init.ts
@@ -1,22 +1,47 @@
-import { initTRPC } from "@trpc/server";
+import { auth } from "@clerk/nextjs/server";
+import { db } from "@/db";
+import { users } from "@/db/schema";
+import { initTRPC, TRPCError } from "@trpc/server";
+import { Redis } from "@upstash/redis";
 import { cache } from "react";
+import SuperJSON from "superjson";
+import { eq } from "drizzle-orm";
 export const createTRPCContext = cache(async () => {
-  /**
-   * @see: https://trpc.io/docs/server/context
-   */
-  return { userId: "user_123" };
+  const { userId } = await auth();
+
+  return { clerkUserId: userId };
 });
-// Avoid exporting the entire t-object
-// since it's not very descriptive.
-// For instance, the use of a t variable
-// is common in i18n libraries.
-const t = initTRPC.create({
-  /**
-   * @see https://trpc.io/docs/server/data-transformers
-   */
-  // transformer: superjson,
+
+export type Context = Awaited<ReturnType<typeof createTRPCContext>>;
+
+const t = initTRPC.context<Context>().create({
+  transformer: SuperJSON,
 });
 // Base router and procedure helpers
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+
+export const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+});
+
+export const protectedProcedure = t.procedure.use(async function isAuthed(
+  options
+) {
+  if (!options.ctx.clerkUserId) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+  const [user] = await db
+    .select()
+    .from(users)
+    .where(eq(users.clerkId, options.ctx.clerkUserId))
+    .limit(1);
+
+  if (!user) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  return options.next({ ctx: { ...options, user } });
+});
