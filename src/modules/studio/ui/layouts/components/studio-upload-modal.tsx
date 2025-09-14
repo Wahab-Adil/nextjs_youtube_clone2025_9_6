e2bd34a98c2fce478dea7335ac18affc1928b8c6
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-09-14 22:44:17 +0430

    onSuccess, onError & Spinner
    Added onSuccess, onError & Spinner  Create Video mutation

--- src/modules/studio/ui/layouts/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/layouts/components/studio-upload-modal.tsx
@@ -1,11 +1,29 @@
 "use client";
+import { trpc } from "@/app/trpc/client";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { Loader2Icon, Plus } from "lucide-react";
+import { toast } from "sonner";
 
 const StudioUploadModal = () => {
+  const utils = trpc.useUtils();
+
+  const createVideo = trpc.videos.create.useMutation({
+    onSuccess: () => {
+      toast.success("video Created.");
+      utils.studio.getMany.invalidate();
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
   return (
-    <Button variant="secondary">
-      <Plus className="size-4" />
+    <Button variant="secondary" onClick={() => createVideo.mutate()}>
+      {createVideo.isPending ? (
+        <Loader2Icon className="animate-spin" />
+      ) : (
+        <Plus className="size-4" />
+      )}
       Create
     </Button>
   );
