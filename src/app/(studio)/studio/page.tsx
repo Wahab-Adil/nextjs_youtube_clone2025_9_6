1d8d78c76b7296a27307e40b6f4a63d2ff83a397
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-09-14 21:13:58 +0430

    Updated studioPage

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,3 +1,12 @@
+import { DEFAULT_LIMIT } from "@/app/constants";
+import { HydrateClient, trpc } from "@/app/trpc/server";
+import StudioView from "@/modules/studio/view/StudioView";
+
 export default function Page() {
-  return <>Studio</>;
+  void trpc.studio.getMany.prefetchInfinite({ limit: DEFAULT_LIMIT });
+  return (
+    <HydrateClient>
+      <StudioView />
+    </HydrateClient>
+  );
 }
