92e20e28095e0306f081fed693c9e54b223d5362
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-09-25 22:22:39 +0430

    thumbnailurl added to schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -36,6 +36,7 @@ export const videos = pgTable("videos", {
   id: uuid("id").primaryKey().defaultRandom(),
   title: text("title").notNull(),
   description: text("description"),
+  thumbnailUrl: text("thumbnail_url"),
   muxStatus: text("mux_status"),
   muxAssetId: text("mux_asset_id").unique(),
   muxUploadId: text("mux_upload_id").unique(),
