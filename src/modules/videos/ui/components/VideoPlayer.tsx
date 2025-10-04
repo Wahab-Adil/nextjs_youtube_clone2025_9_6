5f5abc5d2c2f611bd5ce50b1d1a10bde8ce9650a
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-10-04 21:35:25 +0430

    Create VideoPlayer.tsx

---
+++ src/modules/videos/ui/components/VideoPlayer.tsx
@@ -0,0 +1,33 @@
+"use client";
+import MuxPlayer from "@mux/mux-player-react";
+import { FC } from "react";
+
+interface VideoPlayerProps {
+  playbackId?: string | null | undefined;
+  thumbnailUrl?: string | null | undefined;
+  autoPlay?: string | null;
+  onPlay?: () => void;
+}
+
+export const VideoPlayer = ({
+  playbackId,
+  thumbnailUrl,
+  autoPlay,
+  onPlay,
+}: VideoPlayerProps) => {
+  if (!playbackId) {
+    return;
+  }
+  return (
+    <MuxPlayer
+      playbackId={playbackId}
+      poster={thumbnailUrl ?? "placeholder.svg"}
+      thumbnailTime={0}
+      autoPlay={autoPlay!}
+      onPlay={onplay!}
+      streamType="on-demand"
+      className="w-full h-full object-contain"
+      accentColor="#ff2056"
+    />
+  );
+};
