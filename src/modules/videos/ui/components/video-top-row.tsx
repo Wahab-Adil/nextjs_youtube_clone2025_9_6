1c131e9ea0b31d0f21b3fedc719d351be91a3f2d
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-10-25 22:10:25 +0430

    Update video-top-row.tsx

--- src/modules/videos/ui/components/video-top-row.tsx
+++ src/modules/videos/ui/components/video-top-row.tsx
@@ -1,17 +1,52 @@
+import { useMemo } from "react";
+import { format, formatDistanceToNow } from "date-fns";
+
 import { VideoGetOneOutput } from "../../types";
+
+import VideoMenu from "./video-menu";
 import { VideoOwner } from "./video-owner";
+import VideoDescription from "./video-description";
+import { VideoReactions } from "./video-reactions";
 
 interface VideoTopRowProps {
   video: VideoGetOneOutput;
 }
 
 export const VideoTopRow = ({ video }: VideoTopRowProps) => {
+  const compactViews = useMemo(() => {
+    return Intl.NumberFormat("en", { notation: "compact" }).format(1245000);
+  }, []);
+
+  const expandedViews = useMemo(() => {
+    return Intl.NumberFormat("en", { notation: "standard" }).format(1245000);
+  }, []);
+
+  const compactDate = useMemo(() => {
+    return formatDistanceToNow(new Date(video.createdAt), { addSuffix: true });
+  }, []);
+
+  const expandedDate = useMemo(() => {
+    return format(new Date(video.createdAt), "d MMM yyyy");
+  }, []);
+
   return (
     <div className="flex flex-col gap-4 mt-4">
       <h1 className="text-xl font-semibold ">{video.title}</h1>
       <div className="flex flex-col sm:flex-row sm:items-start sm:justify-between gap-4">
         <VideoOwner user={video.user} videoId={video.id} />
+
+        <div className="flex overflow-x-auto sm:min-w-[calc(50%-6px)] sm:justify-end sm:overflow-visible pb-2 -mb-2 sm:pb-0 sm:mb-0 gap-2">
+          <VideoReactions />
+          <VideoMenu videoId={video.id} variant="secondary" />
+        </div>
       </div>
+      <VideoDescription
+        compactViews={compactViews}
+        expandedViews={expandedViews}
+        compactDate={compactDate}
+        expandedDate={expandedDate}
+        description={video.description}
+      />
     </div>
   );
 };
