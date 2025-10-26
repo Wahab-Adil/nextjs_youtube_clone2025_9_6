e31d924746912fc185cc5780c8b4d389b82ee483
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-10-26 19:40:18 +0430

    createVideoView Trigger & Invalidate video getOne

--- src/modules/videos/ui/sections/Video-section.tsx
+++ src/modules/videos/ui/sections/Video-section.tsx
@@ -1,8 +1,10 @@
 "use client";
-
-import { trpc } from "@/app/trpc/client";
-import { cn } from "@/lib/utils";
 import { Suspense } from "react";
+import { useAuth } from "@clerk/nextjs";
+
+import { cn } from "@/lib/utils";
+import { trpc } from "@/app/trpc/client";
+
 import { ErrorBoundary } from "react-error-boundary";
 import { VideoPlayer } from "../components/VideoPlayer";
 import { VideoBanner } from "../components/video-banner";
@@ -23,7 +25,20 @@ export function VideoSection({ videoId }: videoSectionProps) {
 }
 
 const VideoSectionSuspense = ({ videoId }: videoSectionProps) => {
+  const utils = trpc.useUtils();
+  const { isSignedIn } = useAuth();
   const [video] = trpc.videos.getOne.useSuspenseQuery({ videoId: videoId });
+
+  const createView = trpc.videoViews.create.useMutation({
+    onSuccess: () => {
+      utils.videos.getOne.invalidate({ videoId: videoId });
+    },
+  });
+
+  const handlePlay = () => {
+    if (!isSignedIn) return;
+    createView.mutate({ videoId: videoId });
+  };
 
   return (
     <>
@@ -34,7 +49,7 @@ const VideoSectionSuspense = ({ videoId }: videoSectionProps) => {
         )}
       >
         <VideoPlayer
-          onPlay={() => {}}
+          onPlay={handlePlay}
           playbackId={video.muxPlaybackId}
           thumbnailUrl={video.thumbnailUrl}
         />
