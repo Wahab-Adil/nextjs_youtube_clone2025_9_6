391888d15184ea42ce2837d5205396e873fedb2b
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-10-26 19:35:31 +0430

    integration of CreateView Procedure

--- src/app/trpc/routers/_app.ts
+++ src/app/trpc/routers/_app.ts
@@ -2,9 +2,11 @@ import { createTRPCRouter } from "../init";
 import { videosRouter } from "@/modules/videos/server/procedures";
 import { studioRouter } from "@/modules/studio/server/procedures";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
+import { videoViewsRouter } from "@/modules/video-views/server/procedures";
 export const appRouter = createTRPCRouter({
   studio: studioRouter,
   videos: videosRouter,
+  videoViews: videoViewsRouter,
   categories: categoriesRouter,
 });
 
