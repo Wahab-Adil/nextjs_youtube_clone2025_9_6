e320b588cc4c128f359cce2aa01bc9a6251a288a
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-11-10 20:00:22 +0430

    type of videoGetManyOutput added

--- src/modules/videos/types.ts
+++ src/modules/videos/types.ts
@@ -3,3 +3,6 @@ import { AppRouter } from "@/app/trpc/routers/_app";
 
 export type VideoGetOneOutput =
   inferRouterOutputs<AppRouter>["videos"]["getOne"];
+
+export type videoGetManyOutput =
+  inferRouterOutputs<AppRouter>["suggestion"]["getMany"]["items"];
