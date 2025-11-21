990479a8437b8a9483169ca6577c1791fc6cb011
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-11-21 17:30:56 +0430

    Create procedure.ts

---
+++ src/modules/search/server/procedure.ts
@@ -0,0 +1,58 @@
+import { z } from "zod";
+import { createTRPCRouter, baseProcedure } from "@/app/trpc/init";
+import { db } from "@/db";
+import { videos } from "@/db/schema";
+import { and, desc, eq, ilike, lt, or } from "drizzle-orm";
+
+export const searchRouter = createTRPCRouter({
+  getMany: baseProcedure
+    .input(
+      z.object({
+        query: z.string().nullish(),
+        categoryId: z.string().nullish(),
+        cursor: z
+          .object({
+            id: z.string().uuid(),
+            updatedAt: z.date(),
+          })
+          .nullish(),
+        limit: z.number().min(1).max(100),
+      })
+    )
+    .query(async ({ input }) => {
+      const { cursor, limit, query, categoryId } = input;
+
+      const data = await db
+        .select()
+        .from(videos)
+        .where(
+          and(
+            ilike(videos.title, `%${query}%`),
+            categoryId ? eq(videos.categoryId, categoryId) : undefined,
+            cursor
+              ? or(
+                  lt(videos.updatedAt, cursor.updatedAt),
+                  and(
+                    eq(videos.updatedAt, cursor.updatedAt),
+                    lt(videos.id, cursor.id)
+                  )
+                )
+              : undefined
+          )
+        )
+        .orderBy(desc(videos.updatedAt), desc(videos.id))
+        .limit(limit + 1);
+
+      const hasMore = data.length > limit;
+      const items = hasMore ? data.slice(0, -1) : data;
+      const lastItem = items[items.length - 1];
+      const nextCursor = hasMore
+        ? { id: lastItem.id, updatedAt: lastItem.updatedAt }
+        : null;
+
+      return {
+        items,
+        nextCursor,
+      };
+    }),
+});
