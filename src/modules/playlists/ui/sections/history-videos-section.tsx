1238afaed36cf61591bbbe759b107bea2c06e963
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-11-26 20:39:40 +0430

    history ui Added

---
+++ src/modules/playlists/ui/sections/history-videos-section.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { Suspense } from "react";
+import { trpc } from "@/app/trpc/client";
+import { DEFAULT_LIMIT } from "@/app/constants";
+import { ErrorBoundary } from "react-error-boundary";
+import { InfiniteScroll } from "@/components/infinate-scroll";
+import {
+  VideoGridCard,
+  VideoGridCardSkeleton,
+} from "@/modules/videos/ui/components/video-grid-card";
+import {
+  VideoRowCard,
+  VideoRowCardSkeleton,
+} from "@/modules/videos/ui/components/video-row-card";
+
+export const HistoryVideoSection = () => {
+  return (
+    <Suspense fallback={<HistoryVideoSectionSkeleton />}>
+      <ErrorBoundary fallback={<>Error</>}>
+        <HistoryVideoSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+const HistoryVideoSectionSkeleton = () => {
+  return (
+    <div>
+      <div className="flex flex-col gap-4 gap-y-10 md:hidden">
+        {Array.from({ length: 18 }).map((_, index) => (
+          <VideoGridCardSkeleton key={index} />
+        ))}
+      </div>
+      <div className="hidden flex-col gap-4 md:flex">
+        {Array.from({ length: 18 }).map((_, index) => (
+          <VideoRowCardSkeleton key={index} size="compact" />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const HistoryVideoSuspense = () => {
+  const [data, query] = trpc.playlists.getHistory.useSuspenseInfiniteQuery(
+    { limit: DEFAULT_LIMIT },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+  return (
+    <div>
+      <div className="flex flex-col gap-4 gap-y-10 md:hidden">
+        {data.pages.flatMap((page) =>
+          page.items.map((video) => (
+            <VideoGridCard key={video.id} data={video} />
+          ))
+        )}
+      </div>
+      <div className="hidden flex-col gap-4 md:flex">
+        {data.pages.flatMap((page) =>
+          page.items.map((video) => (
+            <VideoRowCard key={video.id} data={video} size="compact" />
+          ))
+        )}
+      </div>
+      <InfiniteScroll
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
+    </div>
+  );
+};
+
+export default HistoryVideoSuspense;
