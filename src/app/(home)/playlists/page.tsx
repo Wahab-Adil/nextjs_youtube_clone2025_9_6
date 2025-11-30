ba9a7614f1098c46fd20f9440f7f9f3f7ade86b7
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-11-30 15:30:05 +0430

    Hydration & Prefetch done

--- src/app/(home)/playlists/page.tsx
+++ src/app/(home)/playlists/page.tsx
@@ -1,10 +1,16 @@
+import { DEFAULT_LIMIT } from "@/app/constants";
+import { HydrateClient, trpc } from "@/app/trpc/server";
 import { PlaylistsView } from "@/modules/playlists/ui/views/playlists-view";
 
-const page = () => {
+const page = async () => {
+  void (await trpc.playlists.getMany.prefetchInfinite({
+    limit: DEFAULT_LIMIT,
+  }));
+
   return (
-    <div>
+    <HydrateClient>
       <PlaylistsView />
-    </div>
+    </HydrateClient>
   );
 };
 
