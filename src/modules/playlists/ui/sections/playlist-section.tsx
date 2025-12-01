5cdbc31dd059c55b533b24d2eda282189fea08c1
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2025-12-01 20:22:56 +0430

    Update playlist-section.tsx

--- src/modules/playlists/ui/sections/playlist-section.tsx
+++ src/modules/playlists/ui/sections/playlist-section.tsx
@@ -5,9 +5,10 @@ import { trpc } from "@/app/trpc/client";
 import { DEFAULT_LIMIT } from "@/app/constants";
 import { ErrorBoundary } from "react-error-boundary";
 import { InfiniteScroll } from "@/components/infinate-scroll";
-import { VideoGridCardSkeleton } from "@/modules/videos/ui/components/video-grid-card";
-import { VideoRowCardSkeleton } from "@/modules/videos/ui/components/video-row-card";
-import { PlaylistGridCard } from "../components/playlist-grid-card";
+import {
+  PlaylistGridCard,
+  PlaylistGridCardSkeleton,
+} from "../components/playlist-grid-card";
 
 export const PlaylistSection = () => {
   return (
@@ -20,17 +21,15 @@ export const PlaylistSection = () => {
 };
 const PlaylistSectionSkeleton = () => {
   return (
-    <div>
-      <div className="flex flex-col gap-4 gap-y-10 md:hidden">
-        {Array.from({ length: 18 }).map((_, index) => (
-          <VideoGridCardSkeleton key={index} />
-        ))}
-      </div>
-      <div className="hidden flex-col gap-4 md:flex">
-        {Array.from({ length: 18 }).map((_, index) => (
-          <VideoRowCardSkeleton key={index} size="compact" />
-        ))}
-      </div>
+    <div
+      className=" grid gap-4 gap-y-10
+  grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4
+  [@media(min-width:1920px)]:grid-cols-5
+  [@media(min-width:2200px)]:grid-cols-6"
+    >
+      {Array.from({ length: 18 }).map((_, index) => (
+        <PlaylistGridCardSkeleton key={index} />
+      ))}
     </div>
   );
 };
